82e913f0d780f4c2589866bdca172684a77b8754
Zabdi Lopez <zabdi@alumni.uvg.edu.gt>
2024-03-14 13:44:23 +0100

    base_analysis update

--- scripts/data_analysis/base_analysis.R
+++ scripts/data_analysis/base_analysis.R
@@ -1,3 +1,7 @@
+#load libraries
+library(dplyr)
+library(ggplot2)
+
 #E13A####
 ##E13A_01####
 E13A01base_cor_rest <- cor(e13a_bet_n_01_average$E13ABetn01s01, e13a_bet_n_01_average, 
@@ -57,18 +61,47 @@ E13A_03_cor_df_long <- pivot_longer(E13A_03_cor_df, -variable, names_to = "secti
 E13A_03_cor_df_long$category <- ifelse(grepl("^E13ABetn03[oprq]", E13A_03_cor_df_long$section), 
                                        "below_ground", "above_ground")
 
-
-###plot####
-base_e13 <- rbind(
-  data.frame(x="E13A_01", y=E13A_01_cor_df_long$e13a01_correlation, category = E13A_01_cor_df_long$category),
-  data.frame(x="E13A_02", y=E13A_02_cor_df_long$e13a02_correlation, category = E13A_02_cor_df_long$category),
-  data.frame(x="E13A_03", y=E13A_03_cor_df_long$e13a03_correlation, category = E13A_03_cor_df_long$category)
+##create a df ####
+#that contains all three individuals
+base_e13_indv <- rbind(
+  data.frame(x="E13A_01", correlation = E13A_01_cor_df_long$e13a01_correlation,category = E13A_01_cor_df_long$category),
+  data.frame(x="E13A_02", correlation = E13A_02_cor_df_long$e13a02_correlation, category = E13A_02_cor_df_long$category),
+  data.frame(x="E13A_03", correlation =E13A_03_cor_df_long$e13a03_correlation, category = E13A_03_cor_df_long$category)
   )
 
-ggplot(base_e13, aes(x=x, y=y, color = category)) +
+#create a df to unify all individuals
+base_e13_merge <- base_e13_indv
+
+#add a shape column that will be used to separate from individuals in the boxplot for all individuals
+base_e13_merge <- base_e13_merge %>%
+  mutate(shape = case_when(
+    x == "E13A_01" ~ "16",
+    x == "E13A_02" ~ "15",
+    x == "E13A_03" ~ "8",
+    TRUE ~ NA_character_
+  ))
+
+base_e13_indv$shape <- "16"
+
+##plot E13A####
+#Create a df that gathers all the information of the three individuals
+base_e13 <- rbind(
+  data.frame(x=base_e13_indv$x, correlation=base_e13_indv$correlation, category = base_e13_indv$category, shape = base_e13_indv$shape),
+  data.frame(x="E13A", correlation=base_e13_merge$correlation, category = base_e13_merge$category, shape = base_e13_merge$shape)
+  )
+
+base_e13$shape <- factor(base_e13$shape)
+
+#plot the df in boxplots
+color_palette <- c("below_ground" = "#F1BB7B", "above_ground" = "#0B775E")
+
+ggplot(base_e13, aes(x=x, y=correlation, color = category)) +
   geom_boxplot(width=.2, fill = "white", color = "black") +
   geom_jitter(width=0.1) +
+  scale_color_manual(values = color_palette) +
+  # scale_shape_manual(values = unique(base_e13$shape)) +
   labs(x = "Individuals", y = "Base correlation to body parts") +
-  theme_minimal() +
-  facet_wrap(~x, scales="free_x")
+  theme_minimal() 
+  # facet_wrap(~x, scales="free_x")
 
+unique(base_e13$shape)
